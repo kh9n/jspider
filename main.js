5700fdeb1e8b4aca6c140dae0c1467883a913211
Li Jiang <lijia@microsoft.com>
2016-12-22 17:24:19 +0800

    Add gitignore file Add "command-line-args" as dependency Add additional parameter to node for support async and await on node V7

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
-  var argv = process.argv.slice(2);
-  var rawOptions = {}; // map from '--foo' or '-f' to array of values
-  var rawArgs = [];
+const commandLineArgs = require('command-line-args')
+
+const optionDefinitions = [
+    { name: 'setting', alias: 's', type: String },
+    { name: 'help', alias: 'h', type: Boolean, default: true}
+]
+
+const options = commandLineArgs(optionDefinitions);
+
+var printHelp = function() {
+    console.log("this is help");
+}
+
+async function sleep(timeout) {
+  return new Promise((resolve, reject) => {
+    setTimeout(function() {
+      resolve();
+    }, timeout);
+  });
+}
+
+(async function() {
+  console.log('Do some thing, ' + new Date());
+  await sleep(3000);
+  console.log('Do other things, ' + new Date());
+})();
